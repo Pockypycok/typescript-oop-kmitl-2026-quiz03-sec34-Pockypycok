6afd7ea4f4f44eb82f34577e47649870a80370cf
pockypycok <cheophy8@gmail.com>
2026-02-20 18:40:18 +0700

    ooooo

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,1 +1,38 @@
-export class OnlineCourse {}
+import { OnlineCourse } from "./online-course";
+
+const course = new OnlineCourse("TypeScript OOP", 3);
+
+console.log(course.courseName);
+// TypeScript OOP
+
+console.log(course.getAvailableSeats());
+// 3
+
+console.log(course.getCourseStatus());
+// Open
+
+console.log(course.enroll());
+// true
+
+console.log(course.getAvailableSeats());
+// 2
+
+console.log(course.enroll());
+// true
+
+console.log(course.enroll());
+// true
+
+console.log(course.getAvailableSeats());
+// 0
+
+console.log(course.enroll());
+// false  (Course Full)
+
+course.closeCourse();
+
+console.log(course.getCourseStatus());
+// Closed
+
+console.log(course.enroll());
+// false  (Course Closed)
