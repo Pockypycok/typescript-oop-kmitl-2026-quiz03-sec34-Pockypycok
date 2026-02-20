987d5664431d6908ff0967e26b920eaf79f872c3
pockypycok <cheophy8@gmail.com>
2026-02-20 18:06:57 +0700

    lll

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,6 +1,17 @@
-expoort class Car {
-    static 
-}
+export class Car {
+    public brand: string = "";
+    public model: string = "";
 
-  )
-  )
+    start(): void{
+        console.log(`${this.brand} ${this.model} is starting...`);
+    }
+    accelerate(): void{
+        console.log(`${this.brand} ${this.model} is accelerating...`);
+    }
+    brake(): void{
+        console.log(`${this.brand} ${this.model} is braking...`);
+    }
+    stop(): void{
+        console.log(`${this.brand} ${this.model} is stopping...`);
+    }
+}
